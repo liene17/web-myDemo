95ee065596d5010474cdfae536cefa96e3edd053
Liene <rudukaliene@gmail.com>
2020-03-04 21:29:43 +0200

    cats

---
+++ src/main/java/lv/accenture/bootcamp/webdemo/controller/CatController.java
@@ -0,0 +1,13 @@
+package lv.accenture.bootcamp.webdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CatController {
+
+    @GetMapping("/cats")
+    public String catIndex(){
+        return "cats-index";
+    }
+}
---
+++ src/main/java/lv/accenture/bootcamp/webdemo/model/Cat.java
@@ -0,0 +1,33 @@
+package lv.accenture.bootcamp.webdemo.model;
+
+public class Cat {
+
+    private Long id;
+
+    private String nickname;
+
+
+    public Cat() {
+    }
+
+    public Cat(Long id, String nickname) {
+        this.id = id;
+        this.nickname = nickname;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+}
